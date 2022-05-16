ee924d8079106ca2bcc3fe819feea436cf0f1a18
robertaparna <robertaprna@gmail.com>
2022-05-16 17:19:02 +0300

    Changes by andra

--- src/main/java/src/kinosaalihaldur2/SaalideHaldamine.java
+++ src/main/java/src/kinosaalihaldur2/SaalideHaldamine.java
@@ -1,15 +1,15 @@
 package src.kinosaalihaldur2;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -40,7 +40,6 @@ public class SaalideHaldamine {
         hBox.setSpacing(10);
         hBox.setPadding(new Insets(5));
         Text tekst = new Text("Saalide haldamine");
-        tekst.setTextAlignment(TextAlignment.RIGHT);
         tekst.setFont(Font.font(null, FontWeight.BOLD, 20));
         hBox.getChildren().addAll(tagasi,tekst);
 //        hBox.setPrefSize(300,30);
@@ -87,7 +86,9 @@ public class SaalideHaldamine {
 
     }
 
-    public Group getJuur() {
-        return juur;
+
+    public Scene getStseen() {
+        return stseen;
     }
+
 }
